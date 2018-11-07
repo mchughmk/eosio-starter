dc80fe19929589ce2598f054714731c80e2d23c8
Matt McHugh <matt.mchugh@excella.com>
2018-11-07 16:19:04 -0500

    Change cleos aliases to use docker-compose

--- activate.sh
+++ activate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-alias cleos="docker exec -it eosio-sample /opt/eosio/bin/cleos --url http://127.0.0.1:7777 --wallet-url http://127.0.0.1:5555"
+alias cleos="docker-compose exec eosio /opt/eosio/bin/cleos --url http://127.0.0.1:7777 --wallet-url http://127.0.0.1:5555"
 
 prompt=$PS1
 PS1="(eosio-sample) ${prompt}"
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cleos="docker exec -it eosio-sample /opt/eosio/bin/cleos --url http://127.0.0.1:7777 --wallet-url http://127.0.0.1:5555"
+cleos="docker-compose exec eosio /opt/eosio/bin/cleos --url http://127.0.0.1:7777 --wallet-url http://127.0.0.1:5555"
 
 for file in build/contracts/*/ ; do 
   if [[ -d "$file" && ! -L "$file" && "$file" != *"CMakeFiles"* ]]; then
