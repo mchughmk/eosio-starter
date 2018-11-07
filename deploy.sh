2d37be998f28958e9b26c2b5030eb0817ecfb235
Matt McHugh <matt.mchugh@excella.com>
2018-11-07 16:16:06 -0500

    Update deploy.sh with correct file paths for contracts

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@
 
 cleos="docker exec -it eosio-sample /opt/eosio/bin/cleos --url http://127.0.0.1:7777 --wallet-url http://127.0.0.1:5555"
 
-for file in /contracts/*/ ; do 
+for file in build/contracts/*/ ; do 
   if [[ -d "$file" && ! -L "$file" && "$file" != *"CMakeFiles"* ]]; then
-    contract="$(cd "$(dirname "$file")"; pwd)/$(basename "$file")"
+    contract="/contracts/$(basename "$file")"
     $cleos set contract account "$contract" -p account@active
   fi; 
 done
